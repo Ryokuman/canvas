6234c9180dc1d4f98e100ed99bbb1585021293ac
Ryokuman <ryokuman@naver.com>
2023-01-09 19:52:55 +0900

    feature ( textfield ) : add fonts to textfield

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import fonts from "../assets/fonts/fonts";
 
 type propsType = {
   width?: number;
@@ -7,9 +8,11 @@ type propsType = {
 };
 
 const TextField = styled.input.attrs((props: propsType) => ({ placeholder: props.placeholder }))<propsType>`
+  ${fonts}
   width: ${(props) => props.width || "800px"};
   height: ${(props) => props.height || "80px"};
   background-color: "#FFFFFF";
+  font-family: Inter;
 `;
 
 export default TextField;
