503c6e11168a1369e89cbf03da8d2b6ca1b903c4
Ryokuman <ryokuman@naver.com>
2023-01-09 21:23:45 +0900

    feature ( fonts ) : add GloryBold

--- src/assets/fonts/fonts.ts
+++ src/assets/fonts/fonts.ts
@@ -1,11 +1,16 @@
 import { css } from "styled-components";
 import Glory from "./Glory.ttf";
+import GloryBold from "./Glory-Bold.ttf";
 import Inter from "./Inter.ttf";
 
 const fonts = css`
   @font-face {
     font-family: "Glory";
     src: url(${Glory}) format("truetype");
+  }
+  @font-face {
+    font-family: "GloryBold";
+    src: url(${GloryBold}) format("truetype");
   }
   @font-face {
     font-family: "Inter";
