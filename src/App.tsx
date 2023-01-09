ce26180bf7f0a59ee0ea48123f6a8a2ab7142384
Ryokuman <ryokuman@naver.com>
2023-01-09 21:23:25 +0900

    feature ( route ) : add route to Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Login from "@pages/Login";
 
 function App() {
   return (
     <Routes>
       <Route path="" element={<div>hello</div>} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
