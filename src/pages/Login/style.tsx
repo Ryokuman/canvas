b052bec074d06fc35c30b8c9b0e95a67580d93c6
Ryokuman <ryokuman@naver.com>
2023-01-10 04:27:20 +0900

    feature ( Login ) : add style

---
+++ src/pages/Login/style.tsx
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+import TextField from "@components/TextField";
+import Button from "@components/Button";
+import Container from "@components/Container";
+import { Link } from "react-router-dom";
+import Typography from "@components/Typography";
+import fonts from "@assets/fonts/fonts";
+
+const LoginTextField = styled(TextField)`
+  width: ${(props) => props.width || "490px"};
+  height: ${(props) => props.height || "30px"};
+  margin: ${(props) => props.margin || "10px"};
+`;
+
+const LoginButton = styled(Button)`
+  width: ${(props) => props.width || "508px"};
+  height: ${(props) => props.height || "48px"};
+  margin: ${(props) => props.margin || "10px"};
+`;
+
+const LoginContainer = styled(Container)`
+  width: ${(props) => props.width || "auto"};
+  height: ${(props) => props.height || "auto"};
+  margin: ${(props) => props.margin || "10px"};
+`;
+
+const LoginLink = styled(Link)`
+  ${fonts}
+  font-family: InterBold;
+  color: #d33e3e;
+  font-size: 18px;
+  font-weight: bold;
+  text-decoration: none;
+`;
+
+const LoginTypo = styled(Typography)`
+  font-size: ${(props) => props.size || 18}px;
+`;
+
+export { LoginTextField, LoginButton, LoginContainer, LoginTypo, LoginLink };
