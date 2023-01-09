2d9be5d7a86df6c284a0e0dc1066ff5e98951309
Ryokuman <ryokuman@naver.com>
2023-01-10 04:53:19 +0900

    feature ( login ) : add link to signup

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -12,7 +12,7 @@ function Login() {
       <LoginContainer>
         <LoginContainer margin="0" direction="row">
           <LoginTypo margin="0 4px 0 48px">계정이 없으신가요?</LoginTypo>
-          <LoginLink to="./">회원가입</LoginLink>
+          <LoginLink to="../signup">회원가입</LoginLink>
         </LoginContainer>
         <LoginContainer direction="row">
           <LoginTypo margin="0 4px 0 -10px">비밀번호가 기억나지 않으시나요? </LoginTypo>
