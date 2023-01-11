30fb37b0a15dcd18b46fef75058225d59c75a0b2
Ryokuman <ryokuman@naver.com>
2023-01-12 02:56:26 +0900

    feature ( component ) : typography
    feature ( typography ) : add props fonts

--- src/components/Typography.tsx
+++ src/components/Typography.tsx
@@ -4,17 +4,17 @@ import fonts from "@assets/fonts/fonts";
 type propsType = {
   width?: string;
   height?: string;
-  placeholder?: string;
   margin?: string;
   color?: string;
   size?: number;
+  font?: string;
 };
 
 const Typography = styled.p<propsType>`
   ${fonts}
   margin: ${(props) => props.margin};
 
-  font-family: InterBold;
+  font-family: ${(props) => props.font || "InterBold"};
   font-size: ${(props) => (props.size || 2) * 16}px;
   font-weight: bold;
   color: ${(props) => props.color || "#7c7c7e"};
