1950d1a29412e109dbaeca5fe8f4869c6608528c
Ryokuman <ryokuman@naver.com>
2023-01-12 04:25:45 +0900

    feature ( main ) : add Profile to index

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,4 +1,6 @@
+import Container from "@components/Container";
 import Post from "./Post";
+import Profile from "./Profile";
 
 const postData = [
   { user: "ryo_ku_man", comments: 100, tags: ["tag1", "tag2"] },
@@ -6,13 +8,22 @@ const postData = [
   { user: "username", comments: 100, tags: ["tag1", "tag2"] },
 ];
 
+const userData = {
+  name: "user",
+  email: "email@email.com",
+  posts: 100,
+};
+
 function Main() {
   return (
-    <div>
-      {postData.map((e) => (
-        <Post user={e.user} comments={e.comments} tags={e.tags} />
-      ))}
-    </div>
+    <Container direction="row" alginItems="flex-start">
+      <Container>
+        {postData.map((e) => (
+          <Post user={e.user} comments={e.comments} tags={e.tags} />
+        ))}
+      </Container>
+      <Profile user={userData} />
+    </Container>
   );
 }
 
