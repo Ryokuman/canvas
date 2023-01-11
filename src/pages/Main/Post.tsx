272e63d3fbd3230b5fa1ad3e02b90b64025993d3
Ryokuman <ryokuman@naver.com>
2023-01-12 03:23:23 +0900

    chore ( component ) : post
    chore ( post ) : change name of post
    chore ( post ) : change size of username

--- src/pages/Main/Post.tsx
+++ src/pages/Main/Post.tsx
@@ -14,7 +14,9 @@ function Post({ user, comments, tags }: PostProps) {
       <Box margin="0 0 0 10px" color="#d9d9d9">
         <Box width="100px" height="140px" color="#ffffff">
           <Box width="80px" height="80px" color="#d9d9d9"></Box>
-          <Typography margin="10px 0 0 0">{user}</Typography>
+          <Typography margin="10px 0 0 0" size={1}>
+            {user}
+          </Typography>
         </Box>
         <Box margin="16px 0" width="100px" height="30px" direction="row">
           <Typography margin="0 10px 0 0" size={1}>
