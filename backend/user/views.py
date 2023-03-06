c7c6664c8f9566d0ac2278daf6dcb533d033aca0
Ryokuman <ryokuman@naver.com>
2023-03-06 14:59:47 +0900

    feature ( user/views ) : add signup

--- backend/user/views.py
+++ backend/user/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import HttpResponse
 
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from userUtil import user_create
 
 
 @api_view(['GET', 'POST', 'PATCH'])
@@ -21,8 +22,12 @@ def get(request):
 
 
 def post(request):
-    value = request.data('value')
-    return JsonResponse({"result": value}, status=200)
+    name = request.data['name']
+    email = request.data['name']
+    password = request.data['password']
+    user_create(name=name, email=email, password=password)
+
+    return JsonResponse({result: sucess}, status=200)
 
 
 def patch(request):
