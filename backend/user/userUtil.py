352689717642cdb8c072c8fa9d310c8f647ae4b1
Ryokuman <ryokuman@naver.com>
2023-03-06 14:59:25 +0900

    feature ( userUtil ) : make utils
    feature ( user_create ) : user create util
    feature ( user_hash_password ) : hashing password util
    feature ( user_compare_password ) : password checker
    feature ( find ) : finding utils

--- backend/user/userUtil.py
+++ backend/user/userUtil.py
@@ -1,46 +1,10 @@
-from datetime import datetime, timedelta
-
-import bcrypt
-import jwt
-
 from .models import user
-from .serializers import UserSerializer
-from ..settings import SECRET_KEY, ALGORITHM
 
 
-def user_token_to_data(token):
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
-    except jwt.exceptions.ExpiredSignatureError:
-        return "Expired_Token"
-    except jwt.exceptions.DecodeError:
-        return "Invalid_Token"
-    return payload
-
-
-def user_refresh_to_access(refresh_token):
-    try:
-        payload = jwt.decode(refresh_token, SECRET_KEY, algorithms=ALGORITHM)
-        access_token = jwt.encode(
-            {'id': payload.get('id'), 'name': payload.get('name'), 'alias': payload.get('alias'),
-             'email': payload.get('email'), 'type': "access_token",
-             'exp': datetime.utcnow() + timedelta(minutes=5)}, SECRET_KEY, ALGORITHM).decode('utf-8')
-    except jwt.exceptions.ExpiredSignatureError or jwt.exceptions.DecodeError:
-        return False
-    return access_token
-
-
-def user_generate_access_token(user_data):
-    return jwt.encode(
-        {'id': str(user_data.id), 'alias': user_data.alias, 'exp': datetime.utcnow() + timedelta(minutes=30),
-         'type': 'access_token'},
-        SECRET_KEY, ALGORITHM).decode('utf-8')
-
-
-def user_generate_refresh_token(user_data):
-    return jwt.encode({'id': str(user_data.id), 'alias': user_data.alias, 'exp': datetime.utcnow() + timedelta(days=7),
-                       'type': "refresh_token"},
-                      SECRET_KEY, ALGORITHM).decode('utf-8')
+# user create
+def user_create_client(name, email, password):
+    hashed_password, salt = user_hash_password(password)
+    return user.objects.create(name=name, email=email, password=hashed_password, salt=salt)
 
 
 # Password Hashing
@@ -51,62 +15,16 @@ def user_hash_password(password):
     return hash_password, salt
 
 
-class UserDuplicateCheck:
-    @staticmethod
-    def alias(alias):
-        if user_find_by_alias(alias):
-            return False
-        return True
-
-    @staticmethod
-    def email(email):
-        if user_find_by_email(email):
-            return False
-        return True
-
-    @staticmethod
-    def name(name):
-        if user_find_by_name(name):
-            return False
-        return True
+def user_compare_password(password, user_data):
+    password = str(password).encode('utf-8')
+    hash_password = bcrypt.hashpw(password, user_data.salt)
+    return hash_password == user_data.password
 
 
-def user_change_value(value, alias):
-    user_data = user_find_by_alias(alias).first()
-
-    if value.get('password'):
-        hash_password, salt = user_hash_password(value.get('password'))
-        user_data.password = hash_password
-        user_data.salt = salt
-        # value.update({"password": hash_password, "salt": salt})
-    elif value.get('alias'):
-        user_data.alias = value.get('alias')
-    user_data.save()
-    return user_data
-
-
-def user_find_by_id(user_id):
-    return user.objects.filter(id=user_id)
-
-
+# find
 def user_find_by_name(name):
     return user.objects.filter(name=name)
 
 
-def user_find_by_alias(alias):
-    return user.objects.filter(alias=alias)
-
-
 def user_find_by_email(email):
     return user.objects.filter(email=email)
-
-
-def user_create_client(name, email, password, alias):
-    hash_password, salt = user_hash_password(password)
-    return user.objects.create(name=name, alias=alias, password=hash_password, salt=salt, email=email)
-
-
-def user_comppassword(password, user_data):
-    password = str(password).encode('utf-8')
-    hash_password = bcrypt.hashpw(password, user_data.salt)
-    return hash_password == user_data.password
