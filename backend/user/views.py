f5bc5b3b7106792f3542f646421aad2c2f825173
Ryokuman <ryokuman@naver.com>
2023-03-06 16:34:37 +0900

    feature ( use ) : views
    add login function

--- backend/user/views.py
+++ backend/user/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import HttpResponse
 
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from .userUtil import user_create
+from .userUtil import user_create, user_compare_password, user_find_by_email
 
 
 @api_view(['GET', 'POST', 'PATCH'])
@@ -11,7 +11,10 @@ def user(request):
     if request.method == 'GET':
         return get(request)
     if request.method == 'POST':
-        return post(request)
+        if request.data['req'] == "new":
+            return post_new(request)
+        else:
+            return post_login(request)
     if request.method == 'PATCH':
         return patch(request)
 
@@ -21,12 +24,22 @@ def get(request):
     return JsonResponse({"result": value}, status=200)
 
 
-def post(request):
+def post_new(request):
     email = request.data['email']
     password = request.data['password']
     user_create(email=email, password=password)
 
     return JsonResponse({"result": "success"}, status=200)
+
+
+def post_login(request):
+    email = request.data['email']
+    password = request.data['password']
+
+    user_data = user_find_by_email(email=email).first()
+    result = user_compare_password(password=password, user_data=user_data)
+
+    return JsonResponse({"result": result}, status=200)
 
 
 def patch(request):
