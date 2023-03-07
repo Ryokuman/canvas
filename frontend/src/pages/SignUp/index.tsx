456c7c24d21fa947b48a33b7f9e2885dda437b3d
Ryokuman <ryokuman@naver.com>
2023-03-07 18:51:28 +0900

    feature ( signup ) : add signup func

--- frontend/src/pages/SignUp/index.tsx
+++ frontend/src/pages/SignUp/index.tsx
@@ -1,22 +1,57 @@
 import Container from "@components/Container";
-import { SignUpTextField, SignUpButton, SignUpContainer, SignUpTypo, SignUpLink } from "./style";
+import { SignUpTextField, SignUpButton, SignUpContainer, SignUpTypo, SignUpLink, CustomForm } from "./style";
+import { useState } from "react";
 import Logo from "@components/Logo";
+import api from "@utils/api";
+import { useNavigate } from "react-router-dom";
+
+type onSubmit = React.FormEventHandler<HTMLFormElement>;
+type onChange = React.ChangeEventHandler<HTMLInputElement>;
 
 function SignUp() {
+  const [email, setEmail] = useState("");
+  const [pw, setPw] = useState("");
+  const [conPw, setConPw] = useState("");
+  const navigate = useNavigate();
+
+  const onEmailChange: onChange = (e) => setEmail(e.target.value);
+  const onPwChange: onChange = (e) => setPw(e.target.value);
+  const onConPwChange: onChange = (e) => setConPw(e.target.value);
+
+  const onSubmit: onSubmit = async (e) => {
+    e.preventDefault();
+
+    const data = {
+      req: "new",
+      email: email,
+      password: pw,
+    };
+
+    await api
+      .post("user/", data)
+      .then((res) => {
+        window.alert(res.data.result);
+        navigate("../login");
+      })
+      .catch((res) => window.alert(res.response.data.result));
+  };
+
   return (
     <Container width="100%" height="100%" alginItems="center" justifyContent="center">
       <Logo margin="-66px 0 0 0" type={2} width={210} />
-      <SignUpTextField placeholder="E MAIL" />
-      <SignUpTextField placeholder="PASSWORD" />
-      <SignUpTextField placeholder="CONFIRM PASSWORD" />
-      <SignUpButton>회원가입</SignUpButton>
+      <CustomForm onSubmit={onSubmit}>
+        <SignUpTextField placeholder="E MAIL" value={email} onChange={onEmailChange} />
+        <SignUpTextField placeholder="PASSWORD" type="password" value={pw} onChange={onPwChange} />
+        <SignUpTextField placeholder="CONFIRM PASSWORD" type="password" value={conPw} onChange={onConPwChange} />
+        <SignUpButton type="submit">회원가입</SignUpButton>
+      </CustomForm>
       <SignUpContainer>
-        <SignUpContainer margin="0" direction="row">
-          <SignUpTypo margin="0 4px">계정이 이미 있으신가요?</SignUpTypo>
+        <SignUpContainer direction="row">
+          <SignUpTypo margin="0 4px 0 0">계정이 이미 있으신가요?</SignUpTypo>
           <SignUpLink to="../login">로그인</SignUpLink>
         </SignUpContainer>
         <SignUpContainer direction="row">
-          <SignUpTypo margin="0 4px 0 -10px">비밀번호가 기억나지 않으시나요? </SignUpTypo>
+          <SignUpTypo margin="0 4px 0 0">비밀번호가 기억나지 않으시나요? </SignUpTypo>
           <SignUpLink to="../findpassword">비밀번호 찾기</SignUpLink>
         </SignUpContainer>
       </SignUpContainer>
