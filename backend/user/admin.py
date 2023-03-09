1f5118c3bc97d823ea01db7f77600c9015d7b3e3
Ryokuman <ryokuman@naver.com>
2023-03-09 14:25:59 +0900

    feature ( admin ) : add email

--- backend/user/admin.py
+++ backend/user/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import user
+from .models import User, EmailChecker
 
 
-@admin.register(user)
+@admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'password', 'salt', 'email']
+
+
+@admin.register(EmailChecker)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['email', 'value', 'is_valid']
